9ffd90b511829f4930d3e989c4f7b84676f4dbc2
nakul1010 <nakul.amate@gmail.com>
2023-12-15 01:57:15 +0530

    fix: merge test cases into one

--- test/swap/Ord_Marketplace.t.sol
+++ test/swap/Ord_Marketplace.t.sol
@@ -116,69 +116,41 @@ contract OrdMarketPlaceTest is OrdMarketplace, Test {
 
         testLightRelay = new TestLightRelay();
         super.setRelay(testLightRelay);
-        testLightRelay.setDifficultyFromHeaders(dummyProof().bitcoinHeaders);
+        testLightRelay.setDifficultyFromHeaders(ordinalsInfo[0].proof.bitcoinHeaders);
     }
 
-    function dummyProof() public view returns (BitcoinTx.Proof memory) {
-        return BitcoinTx.Proof({
-            merkleProof: hex"0465f99dbe384bbc5d86a5242712e4154958e4b01f595f14b76f873ec349e14a16b17770af2bb48c9b2ce4dddf4631866fe3753e6c54bdcf18dfb2d4fb9983ee58e4f3be92087c843b815bbe1d5d686dc972552f7ffda4342319ceb5bea67ab0f2e463ec8ce8e3f580c5e2470ef20c5b33398ab9fea5ccbd0b3e3f6211305edafa068a28c8ac634df5bbc8064357295373b97db2600745f23ad6ebc87b66b4a8685aa8ff8e69abc5029dbf4b2fa03f05680c7a2c491410b23a5a6b27c5a91b89dac8cdd16a4460ce8ac8d17491025d29336440a133867f938a7f41cc7a64f3f04ac3817c3eb6a6a11dc30850ca4e80f9abbd42268bcc626138bc01639a902713425e7d3aca45647001fb32ff396c07027c5b081325530e74f936e6c4a8078a05f9717efd315534a84d047ee2ff0b2b93159a2b98eabb578af67ef7540a58e488b9c587a994c1a9a86937ad343ea734b7427678e3e6ba0be8f5045ce47e541bbc",
-            txIndexInBlock: 1,
-            bitcoinHeaders: abi.encodePacked(
-                hex"04000000e0879a33a87bf9481385adae91fa9e93713b932cbe8a09030000000000000000ee5ded948d805bb71bee5de25b447c42527898cac93eee1afe04663bb8204b358627fe56f4960618304a7db1",
-                hex"04000000c0de92e7326cb020b59ffc5998405e539863c57da088a7040000000000000000d8e7273d0198ba4f10dfd57d151327c32113fc244fd0587d161a5c5332a53651ed28fe56f4960618b24502cc"
-                )
-        });
-    }
+    function test_placeOrdinalSellOrder() public {
+        uint256 nextOrdinalId;
+        token1.sudoMint(bob, 200);
 
-    function test_placeOrdinalSellOrderMainnet() public {
-        token1.sudoMint(bob, 100);
+        for (uint256 i = 0; i < ordinalsInfo.length; i++) {
+            uint256 expectedPlaceId = nextOrdinalId++;
 
-        // placeOrdinalSellOrder by alice
-        vm.startPrank(alice);
-        vm.expectEmit();
-        emit placeOrdinalSellOrderEvent(0, ordinalsInfo[1].id, address(token1), 100);
-        this.placeOrdinalSellOrder(ordinalsInfo[1].id, ordinalsInfo[1].utxo, address(token1), 100);
+            // placeOrdinalSellOrder by alice
+            vm.startPrank(alice);
+            vm.expectEmit();
+            emit placeOrdinalSellOrderEvent(expectedPlaceId, ordinalsInfo[i].id, address(token1), 100);
+            this.placeOrdinalSellOrder(ordinalsInfo[i].id, ordinalsInfo[i].utxo, address(token1), 100);
 
-        // acceptOrdinalSellOrder by bob
-        vm.startPrank(bob);
-        token1.approve(address(this), 100);
+            uint256 expectedAcceptId = nextOrdinalId++;
 
-        vm.expectEmit();
-        emit acceptOrdinalSellOrderEvent(0, 1, ordinalsInfo[1].requester, address(token1), 100);
-        uint256 acceptId = this.acceptOrdinalSellOrder(0, ordinalsInfo[1].requester);
-        assertEq(acceptId, 1);
+            // acceptOrdinalSellOrder by bob
+            vm.startPrank(bob);
+            token1.approve(address(this), 100);
 
-        // proofOrdinalSellOrder
-        vm.startPrank(alice);
-        vm.expectEmit();
-        emit proofOrdinalSellOrderEvent(1);
-        this.proofOrdinalSellOrder(1, ordinalsInfo[1].info, ordinalsInfo[1].proof);
-        vm.stopPrank();
-    }
+            vm.expectEmit();
+            emit acceptOrdinalSellOrderEvent(
+                expectedPlaceId, expectedAcceptId, ordinalsInfo[i].requester, address(token1), 100
+            );
+            uint256 acceptId = this.acceptOrdinalSellOrder(expectedPlaceId, ordinalsInfo[i].requester);
+            assertEq(expectedAcceptId, expectedAcceptId);
 
-    function test_placeOrdinalSellOrderTestnet() public {
-        token1.sudoMint(bob, 100);
-
-        // placeOrdinalSellOrder by alice
-        vm.startPrank(alice);
-        vm.expectEmit();
-        emit placeOrdinalSellOrderEvent(0, ordinalsInfo[0].id, address(token1), 100);
-        this.placeOrdinalSellOrder(ordinalsInfo[0].id, ordinalsInfo[0].utxo, address(token1), 100);
-
-        // acceptOrdinalSellOrder by bob
-        vm.startPrank(bob);
-        token1.approve(address(this), 100);
-
-        vm.expectEmit();
-        emit acceptOrdinalSellOrderEvent(0, 1, ordinalsInfo[0].requester, address(token1), 100);
-        uint256 acceptId = this.acceptOrdinalSellOrder(0, ordinalsInfo[0].requester);
-        assertEq(acceptId, 1);
-
-        // proofOrdinalSellOrder
-        vm.startPrank(alice);
-        vm.expectEmit();
-        emit proofOrdinalSellOrderEvent(1);
-        this.proofOrdinalSellOrder(1, ordinalsInfo[0].info, ordinalsInfo[0].proof);
-        vm.stopPrank();
+            // proofOrdinalSellOrder
+            vm.startPrank(alice);
+            vm.expectEmit();
+            emit proofOrdinalSellOrderEvent(expectedAcceptId);
+            this.proofOrdinalSellOrder(expectedAcceptId, ordinalsInfo[i].info, ordinalsInfo[i].proof);
+            vm.stopPrank();
+        }
     }
 }
