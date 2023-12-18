c9ebb13b1f45d2c305001d524dbe2169d47da0af
Gregory Hill <gregorydhill@outlook.com>
2023-12-18 15:14:55 +0000

    feat: add helpers to fetch inscriptions for address / utxos
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- sdk/test/ordinals.test.ts
+++ sdk/test/ordinals.test.ts
@@ -1,10 +1,10 @@
 import { assert } from "chai";
 import * as ecc from "tiny-secp256k1";
 import * as ECPairFactory from "ecpair";
-import { RemoteSigner, inscribeText } from "../src/ordinals";
+import { RemoteSigner, inscribeData } from "../src/ordinals";
 import { Network, Psbt, Transaction, address, initEccLib } from "bitcoinjs-lib";
 import { bitcoin } from "bitcoinjs-lib/src/networks";
-import { chunkContent, MAX_CHUNK_SIZE } from "../src/ordinals/commit";
+import { chunkContent, createTextInscription, MAX_CHUNK_SIZE } from "../src/ordinals/commit";
 
 const ECPair = ECPairFactory.default(ecc);
 initEccLib(ecc);
@@ -51,7 +51,7 @@ describe("Ordinal Tests", () => {
         const secret = "fc7458de3d5616e7803fdc81d688b9642641be32fee74c4558ce680cac3d4111";
         const signer = new StaticSigner(secret);
         const toAddress = "bc1pxaneaf3w4d27hl2y93fuft2xk6m4u3wc4rafevc6slgd7f5tq2dqyfgy06";
-        const tx = await inscribeText(signer, toAddress, 1, "Hello World!", 546);
+        const tx = await inscribeData(signer, toAddress, 1, createTextInscription("Hello World!"), 546);
         assert(tx.getId() == "9312bc8a9541dd3e4b22993740ff96449a52dbca00b8be22b2979bb25053f7d6");
     });
 
