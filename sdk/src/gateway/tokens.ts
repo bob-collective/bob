e838f305ac3f82118424281e16a1afd519438fa9
Gregory Hill <gregorydhill@outlook.com>
2024-09-02 14:16:49 +0100

    feat: add logos
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- sdk/src/gateway/tokens.ts
+++ sdk/src/gateway/tokens.ts
@@ -7,6 +7,7 @@ const TOKENS = [
         decimals: 18,
         bob: "0xBBa2eF945D523C4e2608C9E1214C2Cc64D4fc2e2",
         bobSepolia: "0x6744bAbDf02DCF578EA173A9F0637771A9e1c4d0",
+        logoURI: "https://ethereum-optimism.github.io/data/tBTC/logo.svg",
     },
     {
         name: "Wrapped BTC",
@@ -14,6 +15,7 @@ const TOKENS = [
         decimals: 8,
         bob: "0x03C7054BCB39f7b2e5B2c7AcB37583e32D70Cfa3",
         bobSepolia: "0xe51e40e15e6e1496a0981f90Ca1D632545bdB519",
+        logoURI: "https://ethereum-optimism.github.io/data/WBTC/logo.svg",
     },
     {
         name: "sb tBTC v2",
@@ -21,6 +23,7 @@ const TOKENS = [
         decimals: 8,
         bob: "0x2925dF9Eb2092B53B06A06353A7249aF3a8B139e",
         bobSepolia: "",
+        logoURI: "https://ethereum-optimism.github.io/data/tBTC/logo.svg",
     },
     {
         name: "sb Wrapped BTC",
@@ -28,6 +31,7 @@ const TOKENS = [
         decimals: 8,
         bob: "0x5c46D274ed8AbCAe2964B63c0360ad3Ccc384dAa",
         bobSepolia: "",
+        logoURI: "https://ethereum-optimism.github.io/data/WBTC/logo.svg",
     },
     {
         name: "Segment TBTC",
@@ -35,6 +39,7 @@ const TOKENS = [
         decimals: 8,
         bob: "0xD30288EA9873f376016A0250433b7eA375676077",
         bobSepolia: "",
+        logoURI: "https://ethereum-optimism.github.io/data/tBTC/logo.svg",
     },
     {
         name: "Segment WBTC",
@@ -42,6 +47,7 @@ const TOKENS = [
         decimals: 8,
         bob: "0x6265C05158f672016B771D6Fb7422823ed2CbcDd",
         bobSepolia: "",
+        logoURI: "https://ethereum-optimism.github.io/data/WBTC/logo.svg",
     },
     {
         name: "Staked mtBTC",
@@ -49,6 +55,7 @@ const TOKENS = [
         decimals: 18,
         bob: "",
         bobSepolia: "0xc4229678b65e2D9384FDf96F2E5D512d6eeC0C77",
+        logoURI: "https://ethereum-optimism.github.io/data/tBTC/logo.svg",
     },
     {
         name: "Solv BTC",
@@ -56,6 +63,15 @@ const TOKENS = [
         decimals: 18,
         bob: "0x541FD749419CA806a8bc7da8ac23D346f2dF8B77",
         bobSepolia: "",
+        logoURI: "https://raw.githubusercontent.com/bob-collective/bob/master/assets/solvBTC.svg",
+    },
+    {
+        name: "SolvBTC Babylon",
+        symbol: "SolvBTC.BBN",
+        decimals: 18,
+        bob: "0xCC0966D8418d412c599A6421b760a847eB169A8c",
+        bobSepolia: "",
+        logoURI: "https://raw.githubusercontent.com/bob-collective/bob/master/assets/solvBTC.BBN.svg",
     },
     {
         name: "uniBTC",
@@ -63,6 +79,7 @@ const TOKENS = [
         decimals: 8,
         bob: "0x236f8c0a61dA474dB21B693fB2ea7AAB0c803894",
         bobSepolia: "",
+        logoURI: "https://raw.githubusercontent.com/bob-collective/bob/master/assets/uniBTC.svg",
     },
 ];
 
@@ -82,7 +99,7 @@ for (const token of TOKENS) {
         name: token.name,
         symbol: token.symbol,
         decimals: token.decimals,
-        logoURI: "",
+        logoURI: token.logoURI,
     };
 
     const lowerAddressBobSepolia = token.bobSepolia.toLowerCase();
@@ -92,7 +109,7 @@ for (const token of TOKENS) {
         name: token.name,
         symbol: token.symbol,
         decimals: token.decimals,
-        logoURI: "",
+        logoURI: token.logoURI,
     };
 
     SYMBOL_LOOKUP[ChainId.BOB][lowerTokenBob.symbol.toLowerCase()] = lowerTokenBob;
