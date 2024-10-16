4980d3cde1b3de3691eec16765bc8190a4cc571f
Daniel Simão <rui.daniel.simao@gmail.com>
2024-10-16 17:25:53 +0100

    Revert "feat: include mempol, chain and total in getBalance (#390)" (#391)
    This reverts commit 6a9fb0e2db958dcee090e88168d17e472cfefde8.

--- sdk/src/esplora.ts
+++ sdk/src/esplora.ts
@@ -341,7 +341,6 @@ export class EsploraClient {
      *
      * @dev Should return up to 500 UTXOs - depending on the configured limit.
      * @param {string} address - The Bitcoin address to check.
-     * @param {string} [confirmed] - Whether to return only confirmed UTXOs. If omitted, defaults to false.
      * @returns {Promise<Array<UTXO>>} A promise that resolves to an array of UTXOs.
      */
     async getAddressUtxos(address: string, confirmed?: boolean): Promise<Array<UTXO>> {
@@ -389,19 +388,12 @@ export class EsploraClient {
     }
 
     /**
-     * Get the Bitcoin balance for an address, returning both confirmed (on-chain) and unconfirmed (mempool) balances.
-     *
-     * @dev This method returns an object containing the confirmed balance, unconfirmed balance, and the total balance.
-     *      The confirmed balance represents the amount that is on-chain, while the mempool balance includes transactions
-     *      that are pending (unconfirmed). The total is the sum of both confirmed and unconfirmed balances.
+     * Get the balance for an account / address.
      *
      * @param {string} address - The Bitcoin address to check.
-     * @returns {Promise<{ chain: number, mempool: number, total: number }>} A promise that resolves to an object containing:
-     *      - `chain`: The confirmed on-chain balance in satoshis.
-     *      - `mempool`: The unconfirmed balance (in mempool) in satoshis.
-     *      - `total`: The total balance, which is the sum of the confirmed and unconfirmed balances.
+     * @returns {Promise<number>} A promise that resolves to the balance.
      */
-    async getBalance(address: string): Promise<{ chain: number; mempool: number; total: number }> {
+    async getBalance(address: string): Promise<number> {
         const response = await this.getJson<{
             address: string;
             chain_stats: {
@@ -422,13 +414,7 @@ export class EsploraClient {
 
         const chainBalance = response.chain_stats.funded_txo_sum - response.chain_stats.spent_txo_sum;
         const mempoolBalance = response.mempool_stats.funded_txo_sum - response.mempool_stats.spent_txo_sum;
-
-        // Return as an object with chain, mempool, and total fields
-        return {
-            chain: chainBalance,
-            mempool: mempoolBalance,
-            total: chainBalance + mempoolBalance,
-        };
+        return chainBalance + mempoolBalance;
     }
 
     /**
