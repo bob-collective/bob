3c98610cb1853c21a7e52946eec33ddf342a5006
Orlando <orlandothefraser@gmail.com>
2025-05-13 10:20:35 +0100

    chore: test for gateway swap strategy using icecreamswap router

--- contracts/test/gateway/e2e-strategy-tests/IceCreamSwapStrategyForked.sol
+++ contracts/test/gateway/e2e-strategy-tests/IceCreamSwapStrategyForked.sol
@@ -6,15 +6,39 @@ import {stdStorage, StdStorage, Test, console} from "forge-std/Test.sol";
 using stdStorage for StdStorage;
 
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
-import {
-    IceCreamSwapStrategy, IIceCreamSwapAggregatorGuard
-} from "../../../src/gateway/strategy/IceCreamSwapStrategy.sol";
+import {IceCreamSwapStrategy} from "../../../src/gateway/strategy/IceCreamSwapStrategy.sol";
 import {StrategySwapArgs} from "../../../src/gateway/IStrategy.sol";
 import {Constants} from "./Constants.sol";
 import {ForkedStrategyTemplateWbtc} from "./ForkedTemplate.sol";
 
-interface SingletonFactory {
-    function deploy(bytes memory _initCode, bytes32 salt) external returns (address payable);
+// Using a contract for decoding the routing data so that we can use the same calldata slicing that the aggregator does
+contract CalldataDecoder {
+    // function signature must be the same as the aggregator guard
+    function IceCreamSwap()
+        public
+        view
+        returns (
+            uint256 id,
+            address executor,
+            uint256 amountIn,
+            uint256 minAmountOut,
+            address firstTokenReceiver,
+            address recipient,
+            address tokenIn,
+            address tokenOut
+        )
+    {
+        assembly {
+            id := shr(240, calldataload(4))
+            executor := shr(96, calldataload(6))
+            amountIn := shr(128, calldataload(26))
+            minAmountOut := shr(128, calldataload(42))
+            firstTokenReceiver := shr(96, calldataload(59))
+            recipient := shr(96, calldataload(79))
+            tokenIn := shr(96, calldataload(99))
+            tokenOut := shr(96, calldataload(119))
+        }
+    }
 }
 
 // Command to run this contract tests with Foundry:
@@ -22,72 +46,172 @@ interface SingletonFactory {
 contract IceCreamSwapStrategyForkedWbtc is ForkedStrategyTemplateWbtc {
     event TokenOutput(address tokenReceived, uint256 amountOut);
 
-    SingletonFactory internal singletonFactory = SingletonFactory(0xce0042B868300000d44A59004Da54A005ffdcf9f);
-    bytes32 internal deployerSalt = bytes32(uint256(1));
-
-    uint256 public amountIn = 100000; 
-
-    IIceCreamSwapAggregatorGuard iceCreamSwapAggregatorGuard =
-        IIceCreamSwapAggregatorGuard(0xC87De04e2EC1F4282dFF2933A2D58199f688fC3d);
+    IERC20 wbtc = IERC20(Constants.WBTC_ADDRESS);
+    address iceCreamSwapAggregatorGuard = 0xC87De04e2EC1F4282dFF2933A2D58199f688fC3d;
 
     IceCreamSwapStrategy iceCreamSwapStrategy;
 
-    // Test data
-    // IceCreamSwap API call for wbtc -> usdc.e swap: 
-    // https://aggregator.icecreamswap.com/60808?src=0x03C7054BCB39f7b2e5B2c7AcB37583e32D70Cfa3&dst=0xe75D0fB2C24A55cA1e3F96781a2bCC7bdba058F0&amount=100000&from=0x2344D9B3CCBdecc934Bba644deF132F7d21A7566&slippage=0.5&convenienceFee=0
-    bytes swapCalldata =
-        hex"3f0bde25d502e578184bc88eb48485bba23a37b5509578d2ae38000000000000000000000000000186a0000000000000000000000000057650ee08e578184bc88eb48485bba23a37b5509578d2ae3805e9ab742646956ef3ab0c043f3ddf813ab8432403c7054bcb39f7b2e5b2c7acb37583e32d70cfa3e75d0fb2c24a55ca1e3f96781a2bcc7bdba058f06407fec527abad1aafdb9a3b5a2171800c21a2fe2ece0e4b20ab662a9fc222a391b3ccb9ebf0485d05d032ac25d322df992303dca074ee7392c117b92bfd1fc5e25a8f55c2e849492ad7966ea8a0dd9e060402060506530307010b510301ff";
+    address sender = makeAddr("sender");
+    address recipient = makeAddr("recipient");
+
+    function decodeSwapCalldata(bytes memory swapCalldata)
+        internal
+        returns (uint256, address, uint256, uint256, address, address, address, address)
+    {
+        CalldataDecoder calldataDecoder = new CalldataDecoder();
+        (, bytes memory returnData) = address(calldataDecoder).call(swapCalldata);
+        (
+            uint256 id,
+            address executor,
+            uint256 amountIn,
+            uint256 minAmountOut,
+            address firstTokenReceiver,
+            address _recipient,
+            address tokenIn,
+            address tokenOut
+        ) = abi.decode(returnData, (uint256, address, uint256, uint256, address, address, address, address));
+
+        return (id, executor, amountIn, minAmountOut, firstTokenReceiver, _recipient, tokenIn, tokenOut);
+    }
 
     function setUp() public {
         // transferring from the uniswap pool address to the sender
-        super.simulateForkAndTransfer(
-            16900000, address(0x4A1dF9716147b785f3f82019f36f248Ac15DC308), Constants.DUMMY_SENDER, amountIn*2
-        );
+        // We use a fork that is close in time to when the api calls are made to ensure accurate price/liquidity data
+        super.simulateForkAndTransfer(17099000, address(0x4A1dF9716147b785f3f82019f36f248Ac15DC308), sender, 1000000000);
 
-        // Using Create2 to deploy the strategy so we can precompute the address for use in API calls
-        // address is: 0x2344D9B3CCBdecc934Bba644deF132F7d21A7566
-        iceCreamSwapStrategy = IceCreamSwapStrategy(
-            singletonFactory.deploy(
-                abi.encodePacked(type(IceCreamSwapStrategy).creationCode, abi.encode(iceCreamSwapAggregatorGuard)),
-                deployerSalt
-            )
-        );
-
-        console.log("iceCreamSwapStrategy", address(iceCreamSwapStrategy));
+        iceCreamSwapStrategy = new IceCreamSwapStrategy(iceCreamSwapAggregatorGuard);
     }
 
     function testSwap() public {
-        // Initial wbtc balance of the sender
-        uint256 initialWbtcBalance = token.balanceOf(Constants.DUMMY_SENDER);
+        uint256 amountIn = 10000; // 0.0001 wbtc
+        IERC20 lbtc = IERC20(0xA45d4121b3D47719FF57a947A9d961539Ba33204);
 
-        console.log("initialWbtcBalance", initialWbtcBalance);
+        // IceCreamSwap api call for time when test was written for wbtc -> usdc.e swap:
+        // https://aggregator.icecreamswap.com/60808?src=0x03C7054BCB39f7b2e5B2c7AcB37583e32D70Cfa3&dst=0xA45d4121b3D47719FF57a947A9d961539Ba33204&amount=10000&from=0x006217c47ffA5Eb3F3c92247ffFE22AD998242c5&slippage=0.5&convenienceFee=0
+        // tx.data from api response
+        bytes memory swapCalldata =
+            hex"3f0bde25d919e578184bc88eb48485bba23a37b5509578d2ae3800000000000000000000000000002710000000000000000000000000000026df06e578184bc88eb48485bba23a37b5509578d2ae38006217c47ffa5eb3f3c92247fffe22ad998242c503c7054bcb39f7b2e5b2c7acb37583e32d70cfa3a45d4121b3d47719ff57a947a9d961539ba3320445bfa70e8f387da47ffe3dec447bc16f0b9a62612bfd1fc5e25a8f55c2e849492ad7966ea8a0dd9e0604025303050045510301ff";
+        // toAmount from api response
+        uint256 amountOut = 9999;
 
-        console.log(address(iceCreamSwapAggregatorGuard).code.length);
+        (,, uint256 _amountIn, uint256 _minAmountOut,, address _recipient, address _tokenIn, address _tokenOut) =
+            decodeSwapCalldata(swapCalldata);
 
-        console.log("out initial balance", IERC20(0xe75D0fB2C24A55cA1e3F96781a2bCC7bdba058F0).balanceOf(0x05e9ab742646956Ef3Ab0C043F3ddf813ab84324));
+        assertEq(_tokenIn, address(wbtc), "Token in is not wbtc");
+        assertEq(_tokenOut, address(lbtc), "Token out is not lbtc");
+        assertEq(_recipient, recipient, "Recipient is not the expected recipient");
+        assertEq(_amountIn, amountIn, "Amount in is not the expected amount");
+        assertLt(_minAmountOut, amountOut, "Amount out is less than the minimum");
 
-        vm.startPrank(Constants.DUMMY_SENDER);
-        token.approve(address(iceCreamSwapStrategy), amountIn*2);
-        // vm.expectEmit(address(hybridBTCStrategy));
-        // emit TokenOutput(address(boringVault), amountIn);
+        uint256 initialSenderWbtcBalance = wbtc.balanceOf(sender);
+        uint256 initialRecipientLbtcBalance = lbtc.balanceOf(recipient);
+
+        vm.startPrank(sender);
+        wbtc.approve(address(iceCreamSwapStrategy), amountIn);
+        vm.expectEmit(address(iceCreamSwapStrategy));
+        emit TokenOutput(address(lbtc), amountOut);
         iceCreamSwapStrategy.handleGatewayMessageWithSwapArgs(
-            token,
+            wbtc,
             amountIn,
-            Constants.DUMMY_RECEIVER,
-            StrategySwapArgs({tokenOut: address(0), amountOutMin: 0, routingData: swapCalldata})
+            recipient,
+            StrategySwapArgs({tokenOut: address(lbtc), amountOutMin: 0, routingData: swapCalldata})
         );
         vm.stopPrank();
 
-        // // The recipient should have the amountIn in boringVault shares
-        // assertEq(IERC20(boringVault).balanceOf(Constants.DUMMY_RECEIVER), amountIn);
-        // // The strategy should have no balance of the boringVault since it has been transferred to the recipient
-        // assertEq(IERC20(boringVault).balanceOf(address(hybridBTCStrategy)), 0);
-        // // The sender should have the initialWbtcBalance - amountIn in wbtc
-        // assertEq(token.balanceOf(Constants.DUMMY_SENDER), initialWbtcBalance - amountIn);
+        // assert that the recipient has received more than the minAmountOut
+        assertGe(lbtc.balanceOf(recipient), initialRecipientLbtcBalance + _minAmountOut);
+        assertEq(lbtc.balanceOf(address(iceCreamSwapStrategy)), 0);
+        assertEq(wbtc.balanceOf(address(iceCreamSwapStrategy)), 0);
+        assertEq(wbtc.balanceOf(sender), initialSenderWbtcBalance - amountIn);
+    }
+
+    function testSwapMultiHop() public {
+        // wbtc -> usdc.e
+        uint256 amountIn = 10000000; // 0.1 wbtc
+        IERC20 usdc = IERC20(0xe75D0fB2C24A55cA1e3F96781a2bCC7bdba058F0);
+
+        // https://aggregator.icecreamswap.com/60808?src=0x03C7054BCB39f7b2e5B2c7AcB37583e32D70Cfa3&dst=0xe75D0fB2C24A55cA1e3F96781a2bCC7bdba058F0&amount=10000000&from=0x006217c47ffA5Eb3F3c92247ffFE22AD998242c5&slippage=0.5&convenienceFee=0
+        bytes memory swapCalldata =
+            hex"3f0bde25ac37e578184bc88eb48485bba23a37b5509578d2ae3800000000000000000000000000989680000000000000000000000002577bea0a0ce578184bc88eb48485bba23a37b5509578d2ae38006217c47ffa5eb3f3c92247fffe22ad998242c503c7054bcb39f7b2e5b2c7acb37583e32d70cfa3e75d0fb2c24a55ca1e3f96781a2bcc7bdba058f06407fec527abad1aafdb9a3b5a2171800c21a2fe2ece0e4b20ab662a9fc222a391b3ccb9ebf0485d05d032ac25d322df992303dca074ee7392c117b937760492b899398a2b3b86aa9490679cd9d63b2de112389471d577f7bc45c03c7c37f70abca1cc9302f57b2e4d310f3cf432fafa8d0d780ee920467c42000000000000000000000000000000000000062bfd1fc5e25a8f55c2e849492ad7966ea8a0dd9e0e0402a1470605060e0402bace06070606080206090a53030b011c510301ff";
+        uint256 amountOut = 10113771396;
+
+        (,,, uint256 _minAmountOut,,,,) = decodeSwapCalldata(swapCalldata);
+
+        assertLt(_minAmountOut, amountOut, "Amount out is less than the minimum");
+
+        uint256 initialRecipientUsdcBalance = usdc.balanceOf(recipient);
+
+        vm.startPrank(sender);
+        wbtc.approve(address(iceCreamSwapStrategy), amountIn);
+        iceCreamSwapStrategy.handleGatewayMessageWithSwapArgs(
+            wbtc,
+            amountIn,
+            recipient,
+            StrategySwapArgs({tokenOut: address(usdc), amountOutMin: 0, routingData: swapCalldata})
+        );
+        vm.stopPrank();
+
+        assertGe(usdc.balanceOf(recipient), initialRecipientUsdcBalance + _minAmountOut);
+    }
+
+    function testSwapNoCalldata() public {
+        uint256 amountIn = 10000; // 0.0001 wbtc
+        IERC20 lbtc = IERC20(0xA45d4121b3D47719FF57a947A9d961539Ba33204);
+        uint256 amountOutMin = 9900;
+
+        vm.startPrank(sender);
+        wbtc.approve(address(iceCreamSwapStrategy), amountIn);
+        vm.expectRevert("Swap failed");
+        iceCreamSwapStrategy.handleGatewayMessageWithSwapArgs(
+            wbtc,
+            amountIn,
+            recipient,
+            StrategySwapArgs({tokenOut: address(lbtc), amountOutMin: amountOutMin, routingData: ""})
+        );
+        vm.stopPrank();
+    }
+
+    function testSwapSlippageExceeded() public {
+        // wbtc -> usdc.e
+        uint256 amountIn = 10000000; // 0.1 wbtc
+        IERC20 usdc = IERC20(0xe75D0fB2C24A55cA1e3F96781a2bCC7bdba058F0);
+
+        // https://aggregator.icecreamswap.com/60808?src=0x03C7054BCB39f7b2e5B2c7AcB37583e32D70Cfa3&dst=0xe75D0fB2C24A55cA1e3F96781a2bCC7bdba058F0&amount=10000000&from=0x006217c47ffA5Eb3F3c92247ffFE22AD998242c5&slippage=0.5&convenienceFee=0
+        bytes memory swapCalldata =
+            hex"3f0bde25ac37e578184bc88eb48485bba23a37b5509578d2ae3800000000000000000000000000989680000000000000000000000002577bea0a0ce578184bc88eb48485bba23a37b5509578d2ae38006217c47ffa5eb3f3c92247fffe22ad998242c503c7054bcb39f7b2e5b2c7acb37583e32d70cfa3e75d0fb2c24a55ca1e3f96781a2bcc7bdba058f06407fec527abad1aafdb9a3b5a2171800c21a2fe2ece0e4b20ab662a9fc222a391b3ccb9ebf0485d05d032ac25d322df992303dca074ee7392c117b937760492b899398a2b3b86aa9490679cd9d63b2de112389471d577f7bc45c03c7c37f70abca1cc9302f57b2e4d310f3cf432fafa8d0d780ee920467c42000000000000000000000000000000000000062bfd1fc5e25a8f55c2e849492ad7966ea8a0dd9e0e0402a1470605060e0402bace06070606080206090a53030b011c510301ff";
+        uint256 amountOut = 10113771396;
+
+        (,,, uint256 _minAmountOut,,,,) = decodeSwapCalldata(swapCalldata);
+
+        assertLt(_minAmountOut, amountOut, "Amount out is less than the minimum");
+
+        uint256 initialRecipientUsdcBalance = usdc.balanceOf(recipient);
+
+        // before swap is submitted, another large swap is made which causes the market price to increase
+        // so that the swap will revert due to slippage
+        // https://aggregator.icecreamswap.com/60808?src=0x03C7054BCB39f7b2e5B2c7AcB37583e32D70Cfa3&dst=0xe75D0fB2C24A55cA1e3F96781a2bCC7bdba058F0&amount=30000000&from=0x006217c47ffA5Eb3F3c92247ffFE22AD998242c5&slippage=10&convenienceFee=0
+        bytes memory largeSwapCalldata =
+            hex"3f0bde25afafe578184bc88eb48485bba23a37b5509578d2ae3800000000000000000000000001c9c380000000000000000000000006231bb2a50ce578184bc88eb48485bba23a37b5509578d2ae38006217c47ffa5eb3f3c92247fffe22ad998242c503c7054bcb39f7b2e5b2c7acb37583e32d70cfa3e75d0fb2c24a55ca1e3f96781a2bcc7bdba058f0e112389471d577f7bc45c03c7c37f70abca1cc9302f57b2e4d310f3cf432fafa8d0d780ee920467c42000000000000000000000000000000000000066407fec527abad1aafdb9a3b5a2171800c21a2fe2ece0e4b20ab662a9fc222a391b3ccb9ebf0485d05d032ac25d322df992303dca074ee7392c117b937760492b899398a2b3b86aa9490679cd9d63b2d2bfd1fc5e25a8f55c2e849492ad7966ea8a0dd9e0e04026b5b0605060e0702a7d0060809060702060a0953030b0142510301ff";
+        uint256 largeAmountIn = 30000000;
+
+        vm.startPrank(sender);
+        wbtc.approve(address(iceCreamSwapStrategy), largeAmountIn);
+        iceCreamSwapStrategy.handleGatewayMessageWithSwapArgs(
+            wbtc,
+            largeAmountIn,
+            recipient,
+            StrategySwapArgs({tokenOut: address(usdc), amountOutMin: 0, routingData: largeSwapCalldata})
+        );
+
+        // now try the swap, which will revert due to slippage exceeding 0.5%
+        wbtc.approve(address(iceCreamSwapStrategy), amountIn);
+        vm.expectRevert("Swap failed");
+        iceCreamSwapStrategy.handleGatewayMessageWithSwapArgs(
+            wbtc,
+            amountIn,
+            recipient,
+            StrategySwapArgs({tokenOut: address(usdc), amountOutMin: 0, routingData: swapCalldata})
+        );
+        vm.stopPrank();
     }
 }
-
-
-// 95403654
-
-// 101822274
