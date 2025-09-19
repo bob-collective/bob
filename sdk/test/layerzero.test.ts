8344dc54681f3d07741c3431e2a427c55c8fcfc3
orland0x <orlandothefraser@gmail.com>
2025-09-19 23:43:23 +0800

    refactor: max fees to swap for l0 strategy

--- sdk/test/layerzero.test.ts
+++ sdk/test/layerzero.test.ts
@@ -75,11 +75,12 @@ describe('LayerZero Tests', () => {
         const quote = await client.getQuote({
             fromChain: 'bitcoin',
             fromToken: 'bitcoin',
-            toChain: 'base',
+            toChain: 'ethereum',
             toToken: '0x0555E30da8f98308EdB960aa94C0Db47230d2B9c',
             fromUserAddress: 'bc1q6tgkjx4pgc5qda52fsgeuvjrhml5nuawwplejq',
             toUserAddress: '0x2A7f5295ac6e24b6D2ca78d82E3cbf01dDA52745',
             amount: 3000,
+            l0FeeBuffer: 30000,
         });
 
         console.log('quote', quote);
