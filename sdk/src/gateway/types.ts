52a5106f5b17609150eadf695f44841a5745a18b
Nakul <nakul.amate@gmail.com>
2025-10-07 15:34:42 +0530

    feat: offramp orders now provides `offrampRegistryAddress` (#799)

--- sdk/src/gateway/types.ts
+++ sdk/src/gateway/types.ts
@@ -496,6 +496,8 @@ export type OfframpOrder = {
     shouldFeesBeBumped: boolean;
     /** @dev Indicates whether the user can unlock this order (typically if it's still active) */
     canOrderBeUnlocked: boolean;
+    /** @dev The offramp registry address the order is related to */
+    offrampRegistryAddress: Address;
 };
 
 /** @dev Internal, On-chain fetched state of an active/processed/refunded order */
