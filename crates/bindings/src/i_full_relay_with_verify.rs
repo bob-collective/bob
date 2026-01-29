9b1a4a0d994478a4feb509d9fd681d1b8d809439
ns212 <nikolai@interlay.io>
2026-01-29 12:11:07 +0000

    recreate bindings with forge 1.5.1-stable

--- crates/bindings/src/i_full_relay_with_verify.rs
+++ crates/bindings/src/i_full_relay_with_verify.rs
@@ -653,6 +653,7 @@ function addHeaders(bytes memory _anchor, bytes memory _headers) external return
         use alloy::sol_types as alloy_sol_types;
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = (
                 alloy::sol_types::sol_data::Bytes,
                 alloy::sol_types::sol_data::Bytes,
@@ -693,6 +694,7 @@ function addHeaders(bytes memory _anchor, bytes memory _headers) external return
         }
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = (alloy::sol_types::sol_data::Bool,);
             #[doc(hidden)]
             type UnderlyingRustTuple<'a> = (bool,);
@@ -822,6 +824,7 @@ function addHeadersWithRetarget(bytes memory _oldPeriodStartHeader, bytes memory
         use alloy::sol_types as alloy_sol_types;
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = (
                 alloy::sol_types::sol_data::Bytes,
                 alloy::sol_types::sol_data::Bytes,
@@ -871,6 +874,7 @@ function addHeadersWithRetarget(bytes memory _oldPeriodStartHeader, bytes memory
         }
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = (alloy::sol_types::sol_data::Bool,);
             #[doc(hidden)]
             type UnderlyingRustTuple<'a> = (bool,);
@@ -1004,6 +1008,7 @@ function findAncestor(bytes32 _digest, uint256 _offset) external view returns (b
         use alloy::sol_types as alloy_sol_types;
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = (
                 alloy::sol_types::sol_data::FixedBytes<32>,
                 alloy::sol_types::sol_data::Uint<256>,
@@ -1044,6 +1049,7 @@ function findAncestor(bytes32 _digest, uint256 _offset) external view returns (b
         }
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = (alloy::sol_types::sol_data::FixedBytes<32>,);
             #[doc(hidden)]
             type UnderlyingRustTuple<'a> = (alloy::sol_types::private::FixedBytes<32>,);
@@ -1169,6 +1175,7 @@ function findHeight(bytes32 _digest) external view returns (uint256);
         use alloy::sol_types as alloy_sol_types;
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = (alloy::sol_types::sol_data::FixedBytes<32>,);
             #[doc(hidden)]
             type UnderlyingRustTuple<'a> = (alloy::sol_types::private::FixedBytes<32>,);
@@ -1200,6 +1207,7 @@ function findHeight(bytes32 _digest) external view returns (uint256);
         }
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = (alloy::sol_types::sol_data::Uint<256>,);
             #[doc(hidden)]
             type UnderlyingRustTuple<'a> = (
@@ -1318,6 +1326,7 @@ function getBestKnownDigest() external view returns (bytes32);
         use alloy::sol_types as alloy_sol_types;
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = ();
             #[doc(hidden)]
             type UnderlyingRustTuple<'a> = ();
@@ -1351,6 +1360,7 @@ function getBestKnownDigest() external view returns (bytes32);
         }
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = (alloy::sol_types::sol_data::FixedBytes<32>,);
             #[doc(hidden)]
             type UnderlyingRustTuple<'a> = (alloy::sol_types::private::FixedBytes<32>,);
@@ -1465,6 +1475,7 @@ function getCurrentEpochDifficulty() external view returns (uint256);
         use alloy::sol_types as alloy_sol_types;
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = ();
             #[doc(hidden)]
             type UnderlyingRustTuple<'a> = ();
@@ -1498,6 +1509,7 @@ function getCurrentEpochDifficulty() external view returns (uint256);
         }
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = (alloy::sol_types::sol_data::Uint<256>,);
             #[doc(hidden)]
             type UnderlyingRustTuple<'a> = (
@@ -1614,6 +1626,7 @@ function getLastReorgCommonAncestor() external view returns (bytes32);
         use alloy::sol_types as alloy_sol_types;
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = ();
             #[doc(hidden)]
             type UnderlyingRustTuple<'a> = ();
@@ -1647,6 +1660,7 @@ function getLastReorgCommonAncestor() external view returns (bytes32);
         }
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = (alloy::sol_types::sol_data::FixedBytes<32>,);
             #[doc(hidden)]
             type UnderlyingRustTuple<'a> = (alloy::sol_types::private::FixedBytes<32>,);
@@ -1761,6 +1775,7 @@ function getPrevEpochDifficulty() external view returns (uint256);
         use alloy::sol_types as alloy_sol_types;
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = ();
             #[doc(hidden)]
             type UnderlyingRustTuple<'a> = ();
@@ -1794,6 +1809,7 @@ function getPrevEpochDifficulty() external view returns (uint256);
         }
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = (alloy::sol_types::sol_data::Uint<256>,);
             #[doc(hidden)]
             type UnderlyingRustTuple<'a> = (
@@ -1910,6 +1926,7 @@ function getRelayGenesis() external view returns (bytes32);
         use alloy::sol_types as alloy_sol_types;
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = ();
             #[doc(hidden)]
             type UnderlyingRustTuple<'a> = ();
@@ -1941,6 +1958,7 @@ function getRelayGenesis() external view returns (bytes32);
         }
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = (alloy::sol_types::sol_data::FixedBytes<32>,);
             #[doc(hidden)]
             type UnderlyingRustTuple<'a> = (alloy::sol_types::private::FixedBytes<32>,);
@@ -2062,6 +2080,7 @@ function isAncestor(bytes32 _ancestor, bytes32 _descendant, uint256 _limit) exte
         use alloy::sol_types as alloy_sol_types;
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = (
                 alloy::sol_types::sol_data::FixedBytes<32>,
                 alloy::sol_types::sol_data::FixedBytes<32>,
@@ -2105,6 +2124,7 @@ function isAncestor(bytes32 _ancestor, bytes32 _descendant, uint256 _limit) exte
         }
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = (alloy::sol_types::sol_data::Bool,);
             #[doc(hidden)]
             type UnderlyingRustTuple<'a> = (bool,);
@@ -2240,6 +2260,7 @@ function markNewHeaviest(bytes32 _ancestor, bytes memory _currentBest, bytes mem
         use alloy::sol_types as alloy_sol_types;
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = (
                 alloy::sol_types::sol_data::FixedBytes<32>,
                 alloy::sol_types::sol_data::Bytes,
@@ -2286,6 +2307,7 @@ function markNewHeaviest(bytes32 _ancestor, bytes memory _currentBest, bytes mem
         }
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = (alloy::sol_types::sol_data::Bool,);
             #[doc(hidden)]
             type UnderlyingRustTuple<'a> = (bool,);
@@ -2418,6 +2440,7 @@ function verifyHeaderHash(bytes32 _headerHash, uint8 _numConfs) external view;
         use alloy::sol_types as alloy_sol_types;
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = (
                 alloy::sol_types::sol_data::FixedBytes<32>,
                 alloy::sol_types::sol_data::Uint<8>,
@@ -2460,6 +2483,7 @@ function verifyHeaderHash(bytes32 _headerHash, uint8 _numConfs) external view;
         }
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = ();
             #[doc(hidden)]
             type UnderlyingRustTuple<'a> = ();
@@ -2587,6 +2611,7 @@ function verifyProof(bytes memory _header, bytes memory _proof, bytes32 _txId, u
         use alloy::sol_types as alloy_sol_types;
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = (
                 alloy::sol_types::sol_data::Bytes,
                 alloy::sol_types::sol_data::Bytes,
@@ -2642,6 +2667,7 @@ function verifyProof(bytes memory _header, bytes memory _proof, bytes32 _txId, u
         }
         {
             #[doc(hidden)]
+            #[allow(dead_code)]
             type UnderlyingSolTuple<'a> = ();
             #[doc(hidden)]
             type UnderlyingRustTuple<'a> = ();
@@ -2746,6 +2772,7 @@ function verifyProof(bytes memory _header, bytes memory _proof, bytes32 _txId, u
         }
     };
     ///Container for all the [`IFullRelayWithVerify`](self) function calls.
+    #[derive(Clone)]
     #[derive(serde::Serialize, serde::Deserialize)]
     #[derive()]
     pub enum IFullRelayWithVerifyCalls {
@@ -2776,7 +2803,6 @@ function verifyProof(bytes memory _header, bytes memory _proof, bytes32 _txId, u
         #[allow(missing_docs)]
         verifyProof(verifyProofCall),
     }
-    #[automatically_derived]
     impl IFullRelayWithVerifyCalls {
         /// All the selectors of this enum.
         ///
@@ -2799,6 +2825,58 @@ function verifyProof(bytes memory _header, bytes memory _proof, bytes32 _txId, u
             [227u8, 216u8, 216u8, 216u8],
             [228u8, 113u8, 231u8, 44u8],
         ];
+        /// The names of the variants in the same order as `SELECTORS`.
+        pub const VARIANT_NAMES: &'static [&'static str] = &[
+            ::core::stringify!(verifyProof),
+            ::core::stringify!(getCurrentEpochDifficulty),
+            ::core::stringify!(getBestKnownDigest),
+            ::core::stringify!(getPrevEpochDifficulty),
+            ::core::stringify!(findAncestor),
+            ::core::stringify!(findHeight),
+            ::core::stringify!(addHeaders),
+            ::core::stringify!(markNewHeaviest),
+            ::core::stringify!(addHeadersWithRetarget),
+            ::core::stringify!(isAncestor),
+            ::core::stringify!(getLastReorgCommonAncestor),
+            ::core::stringify!(getRelayGenesis),
+            ::core::stringify!(verifyHeaderHash),
+        ];
+        /// The signatures in the same order as `SELECTORS`.
+        pub const SIGNATURES: &'static [&'static str] = &[
+            <verifyProofCall as alloy_sol_types::SolCall>::SIGNATURE,
+            <getCurrentEpochDifficultyCall as alloy_sol_types::SolCall>::SIGNATURE,
+            <getBestKnownDigestCall as alloy_sol_types::SolCall>::SIGNATURE,
+            <getPrevEpochDifficultyCall as alloy_sol_types::SolCall>::SIGNATURE,
+            <findAncestorCall as alloy_sol_types::SolCall>::SIGNATURE,
+            <findHeightCall as alloy_sol_types::SolCall>::SIGNATURE,
+            <addHeadersCall as alloy_sol_types::SolCall>::SIGNATURE,
+            <markNewHeaviestCall as alloy_sol_types::SolCall>::SIGNATURE,
+            <addHeadersWithRetargetCall as alloy_sol_types::SolCall>::SIGNATURE,
+            <isAncestorCall as alloy_sol_types::SolCall>::SIGNATURE,
+            <getLastReorgCommonAncestorCall as alloy_sol_types::SolCall>::SIGNATURE,
+            <getRelayGenesisCall as alloy_sol_types::SolCall>::SIGNATURE,
+            <verifyHeaderHashCall as alloy_sol_types::SolCall>::SIGNATURE,
+        ];
+        /// Returns the signature for the given selector, if known.
+        #[inline]
+        pub fn signature_by_selector(
+            selector: [u8; 4usize],
+        ) -> ::core::option::Option<&'static str> {
+            match Self::SELECTORS.binary_search(&selector) {
+                ::core::result::Result::Ok(idx) => {
+                    ::core::option::Option::Some(Self::SIGNATURES[idx])
+                }
+                ::core::result::Result::Err(_) => ::core::option::Option::None,
+            }
+        }
+        /// Returns the enum variant name for the given selector, if known.
+        #[inline]
+        pub fn name_by_selector(
+            selector: [u8; 4usize],
+        ) -> ::core::option::Option<&'static str> {
+            let sig = Self::signature_by_selector(selector)?;
+            sig.split_once('(').map(|(name, _)| name)
+        }
     }
     #[automatically_derived]
     impl alloy_sol_types::SolInterface for IFullRelayWithVerifyCalls {
@@ -3332,6 +3410,7 @@ function verifyProof(bytes memory _header, bytes memory _proof, bytes32 _txId, u
         }
     }
     ///Container for all the [`IFullRelayWithVerify`](self) events.
+    #[derive(Clone)]
     #[derive(serde::Serialize, serde::Deserialize)]
     #[derive(Debug, PartialEq, Eq, Hash)]
     pub enum IFullRelayWithVerifyEvents {
@@ -3340,7 +3419,6 @@ function verifyProof(bytes memory _header, bytes memory _proof, bytes32 _txId, u
         #[allow(missing_docs)]
         NewTip(NewTip),
     }
-    #[automatically_derived]
     impl IFullRelayWithVerifyEvents {
         /// All the selectors of this enum.
         ///
@@ -3360,6 +3438,36 @@ function verifyProof(bytes memory _header, bytes memory _proof, bytes32 _txId, u
                 100u8, 113u8, 94u8, 74u8, 40u8, 88u8, 246u8, 65u8, 163u8, 245u8,
             ],
         ];
+        /// The names of the variants in the same order as `SELECTORS`.
+        pub const VARIANT_NAMES: &'static [&'static str] = &[
+            ::core::stringify!(NewTip),
+            ::core::stringify!(Extension),
+        ];
+        /// The signatures in the same order as `SELECTORS`.
+        pub const SIGNATURES: &'static [&'static str] = &[
+            <NewTip as alloy_sol_types::SolEvent>::SIGNATURE,
+            <Extension as alloy_sol_types::SolEvent>::SIGNATURE,
+        ];
+        /// Returns the signature for the given selector, if known.
+        #[inline]
+        pub fn signature_by_selector(
+            selector: [u8; 32usize],
+        ) -> ::core::option::Option<&'static str> {
+            match Self::SELECTORS.binary_search(&selector) {
+                ::core::result::Result::Ok(idx) => {
+                    ::core::option::Option::Some(Self::SIGNATURES[idx])
+                }
+                ::core::result::Result::Err(_) => ::core::option::Option::None,
+            }
+        }
+        /// Returns the enum variant name for the given selector, if known.
+        #[inline]
+        pub fn name_by_selector(
+            selector: [u8; 32usize],
+        ) -> ::core::option::Option<&'static str> {
+            let sig = Self::signature_by_selector(selector)?;
+            sig.split_once('(').map(|(name, _)| name)
+        }
     }
     #[automatically_derived]
     impl alloy_sol_types::SolEventInterface for IFullRelayWithVerifyEvents {
@@ -3428,9 +3536,9 @@ See the [wrapper's documentation](`IFullRelayWithVerifyInstance`) for more detai
         N: alloy_contract::private::Network,
     >(
         address: alloy_sol_types::private::Address,
-        provider: P,
+        __provider: P,
     ) -> IFullRelayWithVerifyInstance<P, N> {
-        IFullRelayWithVerifyInstance::<P, N>::new(address, provider)
+        IFullRelayWithVerifyInstance::<P, N>::new(address, __provider)
     }
     /**Deploys this contract using the given `provider` and constructor arguments, if any.
 
@@ -3442,11 +3550,11 @@ For more fine-grained control over the deployment process, use [`deploy_builder`
         P: alloy_contract::private::Provider<N>,
         N: alloy_contract::private::Network,
     >(
-        provider: P,
+        __provider: P,
     ) -> impl ::core::future::Future<
         Output = alloy_contract::Result<IFullRelayWithVerifyInstance<P, N>>,
     > {
-        IFullRelayWithVerifyInstance::<P, N>::deploy(provider)
+        IFullRelayWithVerifyInstance::<P, N>::deploy(__provider)
     }
     /**Creates a `RawCallBuilder` for deploying this contract using the given `provider`
 and constructor arguments, if any.
@@ -3457,8 +3565,8 @@ the bytecode concatenated with the constructor's ABI-encoded arguments.*/
     pub fn deploy_builder<
         P: alloy_contract::private::Provider<N>,
         N: alloy_contract::private::Network,
-    >(provider: P) -> alloy_contract::RawCallBuilder<P, N> {
-        IFullRelayWithVerifyInstance::<P, N>::deploy_builder(provider)
+    >(__provider: P) -> alloy_contract::RawCallBuilder<P, N> {
+        IFullRelayWithVerifyInstance::<P, N>::deploy_builder(__provider)
     }
     /**A [`IFullRelayWithVerify`](self) instance.
 
@@ -3485,7 +3593,6 @@ See the [module-level documentation](self) for all the available methods.*/
         }
     }
     /// Instantiation and getters/setters.
-    #[automatically_derived]
     impl<
         P: alloy_contract::private::Provider<N>,
         N: alloy_contract::private::Network,
@@ -3496,11 +3603,11 @@ See the [wrapper's documentation](`IFullRelayWithVerifyInstance`) for more detai
         #[inline]
         pub const fn new(
             address: alloy_sol_types::private::Address,
-            provider: P,
+            __provider: P,
         ) -> Self {
             Self {
                 address,
-                provider,
+                provider: __provider,
                 _network: ::core::marker::PhantomData,
             }
         }
@@ -3511,9 +3618,9 @@ Returns a new instance of the contract, if the deployment was successful.
 For more fine-grained control over the deployment process, use [`deploy_builder`] instead.*/
         #[inline]
         pub async fn deploy(
-            provider: P,
+            __provider: P,
         ) -> alloy_contract::Result<IFullRelayWithVerifyInstance<P, N>> {
-            let call_builder = Self::deploy_builder(provider);
+            let call_builder = Self::deploy_builder(__provider);
             let contract_address = call_builder.deploy().await?;
             Ok(Self::new(contract_address, call_builder.provider))
         }
@@ -3523,9 +3630,9 @@ and constructor arguments, if any.
 This is a simple wrapper around creating a `RawCallBuilder` with the data set to
 the bytecode concatenated with the constructor's ABI-encoded arguments.*/
         #[inline]
-        pub fn deploy_builder(provider: P) -> alloy_contract::RawCallBuilder<P, N> {
+        pub fn deploy_builder(__provider: P) -> alloy_contract::RawCallBuilder<P, N> {
             alloy_contract::RawCallBuilder::new_raw_deploy(
-                provider,
+                __provider,
                 ::core::clone::Clone::clone(&BYTECODE),
             )
         }
@@ -3562,7 +3669,6 @@ the bytecode concatenated with the constructor's ABI-encoded arguments.*/
         }
     }
     /// Function calls.
-    #[automatically_derived]
     impl<
         P: alloy_contract::private::Provider<N>,
         N: alloy_contract::private::Network,
@@ -3721,7 +3827,6 @@ the bytecode concatenated with the constructor's ABI-encoded arguments.*/
         }
     }
     /// Event filters.
-    #[automatically_derived]
     impl<
         P: alloy_contract::private::Provider<N>,
         N: alloy_contract::private::Network,
