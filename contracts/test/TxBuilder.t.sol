ff4459698794650e4988ae72d811fbd37d580d1a
orland0x <orlandothefraser@gmail.com>
2026-02-05 11:54:21 +0000

    chore: formatting

--- contracts/test/TxBuilder.t.sol
+++ contracts/test/TxBuilder.t.sol
@@ -11,9 +11,8 @@ contract TxBuilderTest is Test {
         bytes32 opReturnData = keccak256(abi.encodePacked(block.timestamp, block.prevrandao, msg.sender));
 
         BitcoinTxBuilder builder = new BitcoinTxBuilder();
-        BitcoinTx.Info memory txInfo = builder.setScript(hex"146142b39c0073672dc382b89a42b29e06368bcabd").setValue(
-            15000
-        ).setOpReturn(abi.encodePacked(opReturnData)).build();
+        BitcoinTx.Info memory txInfo = builder.setScript(hex"146142b39c0073672dc382b89a42b29e06368bcabd")
+            .setValue(15000).setOpReturn(abi.encodePacked(opReturnData)).build();
 
         BitcoinTx.TxOutputsInfo memory resultInfo =
             BitcoinTx.processTxOutputs(txInfo.outputVector, keccak256(hex"146142b39c0073672dc382b89a42b29e06368bcabd"));
