8c287378abace227d94dcc540b0a182b6efb012c
Slava <slava@interlay.io>
2026-03-24 14:39:45 +0300

    chore: run format

--- sdk/src/gateway/client.ts
+++ sdk/src/gateway/client.ts
@@ -272,16 +272,16 @@ export class GatewayApiClient {
             // Check ETH balance and estimate gas for both potential transactions
             const [allowance] = !isAddressEqual(tokenAddress, zeroAddress)
                 ? await publicClient.multicall({
-                    allowFailure: false,
-                    contracts: [
-                        {
-                            address: tokenAddress,
-                            abi: erc20Abi,
-                            functionName: 'allowance',
-                            args: [accountAddress, spenderAddress],
-                        },
-                    ],
-                })
+                      allowFailure: false,
+                      contracts: [
+                          {
+                              address: tokenAddress,
+                              abi: erc20Abi,
+                              functionName: 'allowance',
+                              args: [accountAddress, spenderAddress],
+                          },
+                      ],
+                  })
                 : [maxUint256];
 
             const requiredAmount = BigInt(quote.offramp.inputAmount.amount);
