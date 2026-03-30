43dc19dfffecff996fa54a8fbb91825488d52beb
Slava <slava@interlay.io>
2026-03-30 15:50:48 +0300

    chore: sdk version 5.3.1

--- sdk/src/gateway/index.ts
+++ sdk/src/gateway/index.ts
@@ -2,16 +2,21 @@ export { OkxWalletAdapter } from './adapters/okx-wallet';
 export { ReownWalletAdapter } from './adapters/reown';
 export { ExecuteQuoteResult, GatewayApiClient as GatewaySDK } from './client';
 export {
+    AnyGatewayError,
+    DetailsFor,
     GatewayError,
     GatewayErrorCode,
+    GatewayErrorDetailsMap,
+    isGatewayError,
     type ExceededLimitDetails,
     type InsufficientAmountDetails,
     type InsufficientFundsDetails,
     type InsufficientSwapAmountDetails,
     type NoRouteDetails,
     type SimulationFailedDetails,
-    type UnableToCoverFeesDetails,
+    type UnableToCoverFeesDetails
 } from './error/gateway-error';
 export * from './generated-client';
 export { GatewayQuoteParams, GetQuoteParams } from './types';
 export { formatBtc, parseBtc } from './utils';
+
