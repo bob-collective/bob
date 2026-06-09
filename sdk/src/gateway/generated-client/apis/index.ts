deccfdfec604af88e74e4a30f30dea05b98992db
Slava <slava@interlay.io>
2026-06-09 16:33:57 +0300

    chore: generate v3 api

--- sdk/src/gateway/generated-client/apis/index.ts
+++ sdk/src/gateway/generated-client/apis/index.ts
@@ -2,3 +2,12 @@
 /* eslint-disable */
 export * from './V1Api';
 export * from './V2Api';
+export {
+    type CreateOrderV3Request,
+    type GetMaxSpendableV2Request as GetMaxSpendableV3Request,
+    type GetOrderV2Request as GetOrderV3Request,
+    type GetOrdersV2Request as GetOrdersV3Request,
+    type GetQuoteV3Request,
+    type RegisterTxV3Request,
+    V3Api,
+} from './V3Api';
