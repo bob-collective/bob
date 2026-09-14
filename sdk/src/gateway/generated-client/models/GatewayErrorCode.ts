3e8c6a8070440fe25aa4b077a71415a0fd44e814
Slava Startsev <slava@interlay.io>
2026-09-14 16:58:19 +0300

    feat(sdk): add ROUTE_PRICE_IMPACT_TOO_HIGH gateway error code (#1175)
    Bump @gobob/bob-sdk to 6.0.1.

--- sdk/src/gateway/generated-client/models/GatewayErrorCode.ts
+++ sdk/src/gateway/generated-client/models/GatewayErrorCode.ts
@@ -31,6 +31,7 @@ export const GatewayErrorCode = {
     PerAccountLimitExceeded: 'PER_ACCOUNT_LIMIT_EXCEEDED',
     GlobalLimitExceeded: 'GLOBAL_LIMIT_EXCEEDED',
     ExceededLimit: 'EXCEEDED_LIMIT',
+    RoutePriceImpactTooHigh: 'ROUTE_PRICE_IMPACT_TOO_HIGH',
     InvalidRequest: 'INVALID_REQUEST',
     InvalidOrderArgs: 'INVALID_ORDER_ARGS',
     InvalidAffiliateFee: 'INVALID_AFFILIATE_FEE',
